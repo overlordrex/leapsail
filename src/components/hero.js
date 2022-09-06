112a9e9326c259d46634da661a42d09116e9b0e0
Olamilekan Akindele <olalekan@astract.com.ng>
2022-09-06 12:17:25 +0100

    hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,7 @@
 import React from "react";
-// import { Link } from "react-router-dom";
-// import google from "../img/google.png";
-// import apple from "../img/apple.png";
+import { Link } from "react-router-dom";
+import google from "../img/google.png";
+import apple from "../img/apple.png";
 
 function Hero() {
   return (
@@ -11,10 +11,10 @@ function Hero() {
           <div className="col-md-6 right">
             <h2 className="mb-5">We are revolutionising<br/>your concept of comfort.</h2>
             <h6 className="mb-5">Created with a can do and achievers mindset. A self-onboarding application with features that open you to a new way.</h6>
-            <a className='btn btn-warning text-white px-5 py-3 rounded-pill' href="/">Get Started</a>
+            <a className='btn brand-bg text-white px-5 py-3 rounded-pill' href="/">Get Started</a>
             <div className="mt-5">
-              {/* <Link to="/"><img src={apple} alt="" title="Download on Appstore" className="img-fluid me-3" /></Link> */}
-              {/* <Link to="/"><img src={google} alt="" title="Download on Playstore" className="img-fluid me-3" /></Link> */}
+              <Link to="/"><img src={apple} alt="" title="Download on Appstore" className="img-fluid" width={170}/></Link>
+              <Link to="/"><img src={google} alt="" title="Download on Playstore" className="img-fluid" width={170} /></Link>
             </div>
           </div>
           <div className="col-md-6 left"></div>
