e3957542b9d997db4e27ea4560180c664a7bc20b
Olamilekan Akindele <olalekan@astract.com.ng>
2022-09-15 17:00:37 +0100

    register

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function SignUp() {
   return (
     <section className="login-form">
@@ -9,6 +10,33 @@ function SignUp() {
             <div className="form-box">
               <div className="passform">
                 <h4 className="text-center mb-5">Welcome! Create your account.</h4>
+                <form className=''>
+                  <div className="row">
+                    <div className="col mb-4">
+                      <input type="text" className="form-control" id="formGroupExampleInput" placeholder="First Name" required/>
+                    </div>
+                    <div className="col mb-4">
+                      <input className="form-control" type="text" maxLength="20" placeholder="Last Name" required/>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col mb-4">
+                      <select className="form-select">
+                        <option selected>Male</option>
+                        <option value="2">Female</option>
+                      </select>
+                    </div>
+                    <div className="col mb-4">
+                      <input className="form-control" type="number" min="18" max="39" placeholder="Age" required/>
+                    </div>
+                  </div>
+                  <div className="mb-4">
+                    <input className="form-control" type="tel" maxLength="14" placeholder="Phone Number" required/>
+                  </div>
+                  <div className="col-12">
+                    <button type="button" className="btn brand-bg text-white px-4 py-3 fs-6 rounded-pill" style={{width: "100%"}}>Next</button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
