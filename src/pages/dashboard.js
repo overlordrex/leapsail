e1d7df4b783e344cf1f5202ca9bd7fba447aac6e
overlord <rexatuzie@gmail.com>
2022-10-04 19:20:59 +0400

    fourth commit

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const dashboard = () => {
   const user = JSON.parse(localStorage.getItem('user'));
+
+  const handleClick = () => {
+    localStorage.clear();
+  };
   return (
     <div>
       <h3>HI</h3>
       <h1>{user.name}</h1>
       <h4>welcome to leapsail.</h4>
+      <Link to={'/login'} onClick={handleClick}>
+        back
+      </Link>
     </div>
   );
 };
