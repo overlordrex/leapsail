08aaafe73137c64338fd88fe021e6c670c8e4eff
overlord <rexatuzie@gmail.com>
2022-10-04 16:01:26 +0400

    initial commit

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,8 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
-import { Link } from "react-router-dom";
-import download from "../img/downloads.png";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import download from '../img/downloads.png';
 
 function LoginForm() {
   return (
@@ -14,41 +14,85 @@ function LoginForm() {
               <div className="row">
                 <div className="col-md-6 left-form">
                   <h4 className="mb-4">Hey Dear!</h4>
-                  <h6>We are creating<br/>
-                  revolution in Africa
+                  <h6>
+                    We are creating
+                    <br />
+                    revolution in Africa
                   </h6>
                   <h5 className="mt-5 fig">8,205</h5>
-                  <p className="mt-3">people have already joined,<br/>
-                  join in also!</p>
+                  <p className="mt-3">
+                    people have already joined,
+                    <br />
+                    join in also!
+                  </p>
                   <div className="">
-                    <img src={download} alt="" className="img-fluid desktop left-footer" />
+                    <img
+                      src={download}
+                      alt=""
+                      className="img-fluid desktop left-footer"
+                    />
                   </div>
                 </div>
                 <div className="col-md-6 right-form">
-                  <h5 className="mb-4">Kindly login to your account</h5>
-                  <form className='row'>
+                  <h5 className="mb-4">Kindly login to your account please</h5>
+                  <form className="row">
                     <div className="mb-4">
-                      <input type="email" className="form-control" id="formGroupExampleInput" placeholder="Enter email address" required/>
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="formGroupExampleInput"
+                        placeholder="Enter email address"
+                        required
+                      />
                     </div>
                     <div className="mb-4">
-                      <input id="password" className="form-control" type="password" maxLength="20" placeholder="Enter password" required/>
+                      <input
+                        id="password"
+                        className="form-control"
+                        type="password"
+                        maxLength="20"
+                        placeholder="Enter password"
+                        required
+                      />
                     </div>
                     <div className="col-12">
-                      <button type="button" className="btn brand-bg text-white px-4 py-3 fs-6 rounded-pill" style={{width: "100%"}}>Log in</button>
+                      <button
+                        type="button"
+                        className="btn brand-bg text-white px-4 py-3 fs-6 rounded-pill"
+                        style={{ width: '100%' }}
+                      >
+                        Log in
+                      </button>
                     </div>
                     <div className="mb-4 my-3">
                       <div className="form-check">
-                        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                        <input
+                          className="form-check-input"
+                          type="checkbox"
+                          value=""
+                          id="flexCheckDefault"
+                        />
                         <label className="form-check-label">
                           <h6>Remember me</h6>
                         </label>
                       </div>
-                      <div className="float-end fp" style={{marginTop: "-39px"}}>
-                        <h6><Link to="/forgot-password">Forgot Password?</Link></h6>
+                      <div
+                        className="float-end fp"
+                        style={{ marginTop: '-39px' }}
+                      >
+                        <h6>
+                          <Link to="/forgot-password">Forgot Password?</Link>
+                        </h6>
                       </div>
                     </div>
                     <div className="right-footer">
-                      <a href="#" className="btn btn-outline-warning px-4 py-3 fs-6 rounded-pill" style={{width: "100%"}}>Sign up</a>
+                      <a
+                        href="#"
+                        className="btn btn-outline-warning px-4 py-3 fs-6 rounded-pill"
+                        style={{ width: '100%' }}
+                      >
+                        Sign up
+                      </a>
                       <img src={download} alt="" className="img-fluid mobile" />
                     </div>
                   </form>
@@ -59,7 +103,7 @@ function LoginForm() {
         </div>
       </div>
     </section>
-  )
+  );
 }
 
 export default LoginForm;
